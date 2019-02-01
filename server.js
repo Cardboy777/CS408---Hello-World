daa43fa1b31362be80d588b78748e195e2e3444b
Reid Kippenbrock <mcreido@gmail.com>
2019-02-01 06:03:09 -0500

    Added React Frontend with example API call

--- server.js
+++ server.js
@@ -6,14 +6,26 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 8080;
 
+const router = express.Router();
+
+//Client-callable API call
+router.get("/getTestMessage", (req, res) => {
+    const msg = "David is the one and only match";
+    res.json(msg);
+});
 
 app.use(express.static('public'));
-app.get('/', function(req, res)
-{
-	res.sendFile(__dirname + '/public/index.html');
+
+// append /api for our http requests
+app.use("/api", router);
+
+//do nothing
+app.get('/', function(req, res){
 });
 
 server.listen(port, function()
 {
-    console.log('listening on ' + port);
+    console.log('Server is listening on ' + port);
 });
+
+module.exports = router;
