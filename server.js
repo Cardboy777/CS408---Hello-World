c80e332cf8e9dbfbe6ff957a60da71705bc4670b
PKPurdue <pkrivack@purdue.edu>
2019-02-08 00:22:43 -0500

    site is semi functional
    yes

--- server.js
+++ server.js
@@ -6,14 +6,35 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 8080;
 
+var users = [];
 
-app.use(express.static('public'));
+app.use(express.static('public'));//public
 app.get('/', function(req, res)
 {
-	res.sendFile(__dirname + '/public/index.html');
+	res.sendFile(__dirname + '/public/html/login.html');
 });
 
 server.listen(port, function()
 {
     console.log('listening on ' + port);
+});
+
+
+io.on('connection', function(socket)
+{
+	console.log('a user connected');
+	socket.id = Math.random();
+	
+	users[socket.id] = [];
+	var user = users[socket.id];
+	socket.on('disconnect', function()
+	{
+		delete[users[socket.id]];
+	});
+	
+	socket.on('ValidateToken', function(data)
+	{
+		///no idea how to validate user key
+		socket.emit("ValidateTokenResponse", {"success": false});
+	});
 });
