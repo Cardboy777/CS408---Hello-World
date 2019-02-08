4dc0f7b2386352daf2dfeaa8dc87e59543d2ab68
David Carr <david@jcarr.net>
2019-02-08 18:46:28 -0500

    matches show on page

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ function findMatches(userName){
     var users = [];
     querySnapshot.forEach(function(doc) {
 
-        if(doc.data().user !== userName || doc.data().user.includes(userName) === false){
+        if(doc.data().user != userName || doc.data().user.includes(userName) === false){
           this.push(doc.data());
         }
 
@@ -36,19 +36,22 @@ function findMatches(userName){
 
     return users;
   });
-  console.log(users)
 
-  finalMatches = [];
-  Promise.all([mainUser, users]).then(function(values){
+
+  var finalMatches = [];
+
+  finalMatches = Promise.all([mainUser, users]).then(function(values){
+    var finalMatches = [];
     var mainUser = values[0];
     var users = values[1];
     var scores = []
+
     if(users.length < 100){
       for(i = 0; i < users.length;i++){
         var score = 0;
         for(j = 1; j < 18; j++){
           var str = "answer"+j;
-          if(mainUser[str] === users[i][str]){
+          if(mainUser[str] == users[i][str]){
             score++;
           }
         }
@@ -57,7 +60,8 @@ function findMatches(userName){
           "score" : score,
           "desc" : users[i]["describe"],
           "gender" : users[i]["gender"],
-          "age" : users[i]["age"]
+          "age" : users[i]["age"],
+          "usersPQID" : users[i]["usersPQID"]
         };
         scores.push(thisScore);
       }
@@ -68,7 +72,8 @@ function findMatches(userName){
           var score = 0;
           for(j = 1; j < 18; j++){
             var str = "answer"+j;
-            if(mainUser[str] === users[random][str]){
+
+            if(mainUser[str] == users[random][str]){
               score++;
             }
           }
@@ -77,10 +82,12 @@ function findMatches(userName){
             "score" : score,
             "desc" : users[i]["describe"],
             "gender" : users[i]["gender"],
-            "age" : users[i]["age"]
+            "age" : users[i]["age"],
+            "usersPQID" : users[i]["usersPQID"]
           };
           users.splice(random, 1);
           scores.push(thisScore);
+
         }
     }
     for(i = 0; i < 10; i++){
@@ -92,21 +99,26 @@ function findMatches(userName){
       for(j = 0; j < scores.length; j++){
         if(scores[j]["score"] > highMatch["score"]){
           highMatch = {
-            "user" : scores[i]["user"],
-            "score" : scores[i]["score"],
-            "desc" : scores[i]["describe"],
+            "name" : scores[i]["user"],
+            "match_percent" : Math.floor(scores[i]["score"]*100/17),
+            "description" : scores[i]["describe"],
             "gender" : scores[i]["gender"],
-            "age" : scores[i]["age"]
+            "age" : scores[i]["age"],
+            "uid" : i,
+            "fav_lang" : "TEST"
           };
           matchNumber = j;
         }
       }
-      this.push(highMatch);
-
+      scores.splice(matchNumber, 1);
+      finalMatches.push(highMatch);
+      if(scores.length === 0) {
+        break;
+      }
     }
+    return finalMatches;
 
-
-  }, finalMatches);
+  });
   return finalMatches;
 }
 
@@ -126,15 +138,19 @@ router.get("/getTestMessage", (req, res) => {
 //NEEDS REAL IMPLEMENTATION, RETURNING DUMMY DATA FOR THE MOMENT
 router.get("/getMorePotentialMatches", (req, res) => {
     //const users = getPotentialMatches(5);
-    findMatches("DavidC")
-    const users = [
-        {uid: 12345, name: "John Smith", description: "I like long walks on the beach", fav_lang: "Java", match_percent: 92},
-        {uid: 12341234, name: "John Doe", description: "I like People named David", fav_lang: "Ruby on Rails", match_percent: 73},
-        {uid: 234432432, name: "Jane Does", description: "I'm an engineer", fav_lang: "MatLab", match_percent:60},
-        {uid: 324363, name: "Yo Low", description: "I will try anything", fav_lang: "HTML", match_percent:45},
-        {uid: 35234, name: "Cardboy777", description: "I like card games", fav_lang: "Visual Basic", match_percent:8}
-    ];
-    res.json(users);
+    result = findMatches("DavidC");
+    result.then(function(ret){
+      console.log(result);
+      const users = [
+          {uid: 12345, name: "John Smith", description: "I like long walks on the beach", fav_lang: "Java", match_percent: 92},
+          {uid: 12341234, name: "John Doe", description: "I like People named David", fav_lang: "Ruby on Rails", match_percent: 73},
+          {uid: 234432432, name: "Jane Does", description: "I'm an engineer", fav_lang: "MatLab", match_percent:60},
+          {uid: 324363, name: "Yo Low", description: "I will try anything", fav_lang: "HTML", match_percent:45},
+          {uid: 35234, name: "Cardboy777", description: "I like card games", fav_lang: "Visual Basic", match_percent:8}
+      ];
+      res.json(ret);
+    });
+
 });
 
 app.use(express.static('public'));
