41122993d7b1cf3e76f6d3cee7cc1d857a1185ae
David Carr <david@jcarr.net>
2019-02-08 10:22:17 -0500

    Basic match algorithm

--- server.js
+++ server.js
@@ -9,7 +9,106 @@ var admin = require('firebase-admin');
 var serviceAccount = require('./serviceAccountKey.json');
 var port = process.env.PORT || 8080;
 
-var users = [];
+
+function findMatches(userName){
+  var mainUser;
+  var users = [];
+  var db = admin.firestore();
+  //Query to get the quiz data for the user
+  mainUser = db.collection("usersPQ").where("user", "==", userName)
+  .get()
+  .then(function(querySnapshot) {
+    return querySnapshot.docs[0].data()
+  });
+
+  //Query to get the quiz data for all the other users
+  users = db.collection("usersPQ")
+  .get()
+  .then(function(querySnapshot) {
+    var users = [];
+    querySnapshot.forEach(function(doc) {
+
+        if(doc.data().user !== userName || doc.data().user.contains(userName) === false){
+          this.push(doc.data());
+        }
+
+    }, users);
+
+    return users;
+  });
+  console.log(users)
+
+  finalMatches = [];
+  Promise.all([mainUser, users]).then(function(values){
+    var mainUser = values[0];
+    var users = values[1];
+    var scores = []
+    if(users.length < 100){
+      for(i = 0; i < users.length;i++){
+        var score = 0;
+        for(j = 1; j < 18; j++){
+          var str = "answer"+j;
+          if(mainUser[str] === users[i][str]){
+            score++;
+          }
+        }
+        thisScore = {
+          "user" : users[i]["user"],
+          "score" : score,
+          "desc" : users[i]["describe"],
+          "gender" : users[i]["gender"],
+          "age" : users[i]["age"]
+        };
+        scores.push(thisScore);
+      }
+    } else {
+        var count = 0;
+        while(count < 100){
+          var random = Math.floor(Math.random() * users.length);
+          var score = 0;
+          for(j = 1; j < 18; j++){
+            var str = "answer"+j;
+            if(mainUser[str] === users[random][str]){
+              score++;
+            }
+          }
+          thisScore = {
+            "user" : users[i]["user"],
+            "score" : score,
+            "desc" : users[i]["describe"],
+            "gender" : users[i]["gender"],
+            "age" : users[i]["age"]
+          };
+          users.splice(random, 1);
+          scores.push(thisScore);
+        }
+    }
+    for(i = 0; i < 10; i++){
+      var highMatch = {
+        "user" : "temp",
+        "score" : -1
+      };
+      var matchNumber = -1;
+      for(j = 0; j < scores.length; j++){
+        if(scores[j]["score"] > highMatch["score"]){
+          highMatch = {
+            "user" : scores[i]["user"],
+            "score" : scores[i]["score"],
+            "desc" : scores[i]["describe"],
+            "gender" : scores[i]["gender"],
+            "age" : scores[i]["age"]
+          };
+          matchNumber = j;
+        }
+      }
+      this.push(highMatch);
+
+    }
+
+
+  }, finalMatches);
+  return finalMatches;
+}
 
 //Database access
 admin.initializeApp({
@@ -27,7 +126,7 @@ router.get("/getTestMessage", (req, res) => {
 //NEEDS REAL IMPLEMENTATION, RETURNING DUMMY DATA FOR THE MOMENT
 router.get("/getMorePotentialMatches", (req, res) => {
     //const users = getPotentialMatches(5);
-
+    findMatches("DavidC")
     const users = [
         {uid: 12345, name: "John Smith", description: "I like long walks on the beach", fav_lang: "Java", match_percent: 92},
         {uid: 12341234, name: "John Doe", description: "I like People named David", fav_lang: "Ruby on Rails", match_percent: 73},
@@ -56,14 +155,14 @@ io.on('connection', function(socket)
 {
 	console.log('a user connected');
 	socket.id = Math.random();
-	
+
 	users[socket.id] = [];
 	var user = users[socket.id];
 	socket.on('disconnect', function()
 	{
 		delete[users[socket.id]];
 	});
-	
+
 	socket.on('ValidateToken', function(data)
 	{
 		///no idea how to validate user key
