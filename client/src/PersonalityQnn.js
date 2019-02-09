8150a1c6fcea25b8b15b2240e4375acf6431dcd7
lkristie <lauragkristie96@gmail.com>
2019-02-08 21:04:02 -0500

    update variable names on personality

--- client/src/PersonalityQnn.js
+++ client/src/PersonalityQnn.js
@@ -14,23 +14,23 @@ class PersonalityQuestionnaire extends Component {
             age:"",
             location:"",
             attrgender:"",
-            a1:"",
-            a2:"",
-            a3:"",
-            a4:"",
-            a5:"",
-            a6:"",
-            a7:"",
-            a8:"",
-            a9:"",
-            a10:"",
-            a11:"",
-            a12:"",
-            a13:"",
-            a14:"",
-            a15:"",
-            a16:"",
-            a17:"",
+            pa1:"",
+            pa2:"",
+            pa3:"",
+            pa4:"",
+            pa5:"",
+            pa6:"",
+            pa7:"",
+            pa8:"",
+            pa9:"",
+            pa10:"",
+            pa11:"",
+            pa12:"",
+            pa13:"",
+            pa14:"",
+            pa15:"",
+            pa16:"",
+            pa17:"",
             description:""
         }
         this.handleChange = this.handleChange.bind(this);
@@ -57,23 +57,23 @@ class PersonalityQuestionnaire extends Component {
             age: this.state.age,
             location: this.state.location,
             attractGender: this.state.attrgender,
-            answer1: this.state.a1,
-            answer2: this.state.a2,
-            answer3: this.state.a3,
-            answer4: this.state.a4,
-            answer5: this.state.a5,
-            answer6: this.state.a6,
-            answer7: this.state.a7,
-            answer8: this.state.a8,
-            answer9: this.state.a9,
-            answer10: this.state.a10,
-            answer11: this.state.a11,
-            answer12: this.state.a12,
-            answer13: this.state.a13,
-            answer14: this.state.a14,
-            answer15: this.state.a15,
-            answer16: this.state.a16,
-            answer17: this.state.a17,
+            panswer1: this.state.pa1,
+            panswer2: this.state.pa2,
+            panswer3: this.state.pa3,
+            panswer4: this.state.pa4,
+            panswer5: this.state.pa5,
+            panswer6: this.state.pa6,
+            panswer7: this.state.pa7,
+            panswer8: this.state.pa8,
+            panswer9: this.state.pa9,
+            panswer10: this.state.pa10,
+            panswer11: this.state.pa11,
+            panswer12: this.state.pa12,
+            panswer13: this.state.pa13,
+            panswer14: this.state.pa14,
+            panswer15: this.state.pa15,
+            panswer16: this.state.pa16,
+            panswer17: this.state.pa17,
             describe: this.state.description
         }
         questionnaireRef.push(questionnaire);
@@ -83,23 +83,23 @@ class PersonalityQuestionnaire extends Component {
             age:"",
             location:"",
             attrgender:"",
-            a1:"",
-            a2:"",
-            a3:"",
-            a4:"",
-            a5:"",
-            a6:"",
-            a7:"",
-            a8:"",
-            a9:"",
-            a10:"",
-            a11:"",
-            a12:"",
-            a13:"",
-            a14:"",
-            a15:"",
-            a16:"",
-            a17:"",
+            pa1:"",
+            pa2:"",
+            pa3:"",
+            pa4:"",
+            pa5:"",
+            pa6:"",
+            pa7:"",
+            pa8:"",
+            pa9:"",
+            pa10:"",
+            pa11:"",
+            pa12:"",
+            pa13:"",
+            pa14:"",
+            pa15:"",
+            pa16:"",
+            pa17:"",
             description:""
         });
     }
@@ -123,23 +123,23 @@ class PersonalityQuestionnaire extends Component {
                     age: questionnaire[q].age,
                     location: questionnaire[q].location,
                     attractGender: questionnaire[q].attractGender,
-                    answer1: questionnaire[q].answer1,
-                    answer2: questionnaire[q].answer2,
-                    answer3: questionnaire[q].answer3,
-                    answer4: questionnaire[q].answer4,
-                    answer5: questionnaire[q].answer5,
-                    answer6: questionnaire[q].answer6,
-                    answer7: questionnaire[q].answer7,
-                    answer8: questionnaire[q].answer8,
-                    answer9: questionnaire[q].answer9,
-                    answer10: questionnaire[q].answer10,
-                    answer11: questionnaire[q].answer11,
-                    answer12: questionnaire[q].answer12,
-                    answer13: questionnaire[q].answer13,
-                    answer14: questionnaire[q].answer14,
-                    answer15: questionnaire[q].answer15,
-                    answer16: questionnaire[q].answer16,
-                    answer17: questionnaire[q].answer17,
+                    panswer1: questionnaire[q].panswer1,
+                    panswer2: questionnaire[q].panswer2,
+                    panswer3: questionnaire[q].panswer3,
+                    panswer4: questionnaire[q].panswer4,
+                    panswer5: questionnaire[q].panswer5,
+                    panswer6: questionnaire[q].panswer6,
+                    panswer7: questionnaire[q].panswer7,
+                    panswer8: questionnaire[q].panswer8,
+                    panswer9: questionnaire[q].panswer9,
+                    panswer10: questionnaire[q].panswer10,
+                    panswer11: questionnaire[q].panswer11,
+                    panswer12: questionnaire[q].panswer12,
+                    panswer13: questionnaire[q].panswer13,
+                    panswer14: questionnaire[q].panswer14,
+                    panswer15: questionnaire[q].panswer15,
+                    panswer16: questionnaire[q].panswer16,
+                    panswer17: questionnaire[q].panswer17,
                     describe: questionnaire[q].description 
                     //
                 });
@@ -196,51 +196,51 @@ class PersonalityQuestionnaire extends Component {
                 </label>
                 <br />
                 <label>What motivates you?
-                    <select class="form-control" id="a1" name="a1" onChange={this.handleChange} value={this.state.a1}>
+                    <select class="form-control" id="pa1" name="pa1" onChange={this.handleChange} value={this.state.pa1}>
                         <option selected></option><option>Inner Peace</option> <option>Success</option> <option>Family</option>
                         <option>Power</option> <option>Competition</option>
                     </select>
                 </label>
                 <br />
                 <label>What do you value the most?
-                    <select class="form-control" id="a2" name="a2" onChange={this.handleChange} value={this.state.a2}>
+                    <select class="form-control" id="pa2" name="pa2" onChange={this.handleChange} value={this.state.pa2}>
                         <option selected></option><option>Spontaneity</option> <option>Wellness</option> <option>Intelligence</option>
                         <option>Fun</option> <option>Loyalty</option>
                     </select>
                 </label>
                 <br />
                 <label>Would you call yourself an introvert or an extrovert?
-                    <select class="form-control" id="a3" name="a3" onChange={this.handleChange} value={this.state.a3}>
+                    <select class="form-control" id="pa3" name="pa3" onChange={this.handleChange} value={this.state.pa3}>
                         <option selected></option><option>Extrovert</option><option>Introvert</option> 
                     </select>
                 </label>
                 <br />
                 <label>Do you enjoy meeting new people?
-                    <select class="form-control" id="a4"name="a4" onChange={this.handleChange} value={this.state.a4}>
+                    <select class="form-control" id="pa4"name="pa4" onChange={this.handleChange} value={this.state.pa4}>
                         <option selected></option><option>Yes</option><option>No</option> 
                     </select>
                 </label>
                 <br />
                 <label>Do you like helping people?
-                    <select class="form-control" id="a5" name="a5" onChange={this.handleChange} value={this.state.a5}>
+                    <select class="form-control" id="pa5" name="pa5" onChange={this.handleChange} value={this.state.pa5}>
                         <option selected></option><option>Yes</option><option>No</option> 
                     </select>            
                 </label>
                 <br />
                 <label>What types of activities do you prefer?
-                    <select class="form-control" id="a6" name="a6" onChange={this.handleChange} value={this.state.a6}>
+                    <select class="form-control" id="pa6" name="pa6" onChange={this.handleChange} value={this.state.pa6}>
                         <option selected></option><option>Indoor</option><option>Outdoor</option><option>Both</option> 
                     </select>
                 </label>
                 <br />
                 <label>Do you enjoy listetning to music?
-                    <select class="form-control" id="a7" name="a7" onChange={this.handleChange} value={this.state.a7}>
+                    <select class="form-control" id="pa7" name="pa7" onChange={this.handleChange} value={this.state.pa7}>
                         <option selected></option><option>Yes, Love it!</option><option>Occasionally</option><option>Not really</option> <option>I hate it</option> 
                     </select>
                 </label>
                 <br />
                 <label> When do you feel like you get the most done?
-                    <select class="form-control" id="a8" name="a8" onChange={this.handleChange} value={this.state.a8}>
+                    <select class="form-control" id="pa8" name="pa8" onChange={this.handleChange} value={this.state.pa8}>
                         <option selected></option>
                         <option>Early Morning</option><option>Late Night</option><option>Between Tasks</option>
                         <option>Whenever Inspiration hits</option> <option>When I have planed a specific time to work on it</option>
@@ -248,56 +248,56 @@ class PersonalityQuestionnaire extends Component {
                 </label>
                 <br />
                 <label>Do you find it easy to stay relaxed when there is some pressure?
-                    <select class="form-control" id="a9" name="a9" onChange={this.handleChange} value={this.state.a9}>
+                    <select class="form-control" id="pa9" name="pa9" onChange={this.handleChange} value={this.state.pa9}>
                         <option selected></option><option>Yes, always</option><option>Most of the time</option><option> Sometimes</option> 
                         <option>Not so much, but I try too</option> <option>No</option>
                     </select>
                 </label>
                 <br />
                 <label>Do you prefer variety to routine?
-                    <select class="form-control" id="a10" name="a10" onChange={this.handleChange} value={this.state.a10}>
+                    <select class="form-control" id="pa10" name="pa10" onChange={this.handleChange} value={this.state.pa10}>
                         <option selected></option><option>Variety</option><option>Routine</option>
                     </select>
                 </label>
                 <br />
                 <label>Do you love to solve complex problems?
-                    <select class="form-control" id="a11" name="a11" onChange={this.handleChange} value={this.state.a11}>
+                    <select class="form-control" id="pa11" name="pa11" onChange={this.handleChange} value={this.state.pa11}>
                         <option selected></option><option>Yes, Love it!</option><option>Sometimes</option><option>Not really</option> <option>I hate it</option> 
                     </select>
                 </label>
                 <br />
                 <label>Do you like to be in the center of attention?
-                    <select class="form-control" id="a12" name="a12" onChange={this.handleChange} value={this.state.a12}>
+                    <select class="form-control" id="pa12" name="pa12" onChange={this.handleChange} value={this.state.pa12}>
                         <option selected></option><option>Yes, Love it!</option><option>Sometimes</option><option>It depends on the situation</option><option>Not really</option> <option>I hate it</option> 
                     </select>
                 </label>
                 <br />
                 <label>How do you feel about trying new things?
-                    <select class="form-control" id="a13" name="a13" onChange={this.handleChange} value={this.state.a13}>
+                    <select class="form-control" id="pa13" name="pa13" onChange={this.handleChange} value={this.state.pa13}>
                         <option selected></option><option>I love trying new things!</option><option>I'm okay at it</option><option>It depends on the thing</option> <option>I'd rather not if I could</option> 
                     </select>
                 </label>
                 <br />
                 <label>Would you describe yourself as a go-getter?
-                    <select class="form-control" id="a14" name="a14" onChange={this.handleChange} value={this.state.a14}>
+                    <select class="form-control" id="pa14" name="pa14" onChange={this.handleChange} value={this.state.pa14}>
                         <option selected></option><option>Yes, of course</option><option>Sometimes, when I need to be</option><option>Not really</option> 
                     </select>
                 </label>
                 <br />
                 <label>How do you feel about large parties?
-                    <select class="form-control" id="a15" name="a15" onChange={this.handleChange} value={this.state.a15}>
+                    <select class="form-control" id="pa15" name="pa15" onChange={this.handleChange} value={this.state.pa15}>
                         <option selected></option><option>I love them!</option> <option>They are fun every once in a while</option> <option>Not really a fan of them</option> <option>I hate them</option> 
                     </select>
                 </label>
                 <br />
                 <label>Do you enjoy being part of a group?
-                    <select class="form-control" id="a16" name="a16" onChange={this.handleChange} value={this.state.a16}>
+                    <select class="form-control" id="pa16" name="pa16" onChange={this.handleChange} value={this.state.pa16}>
                         <option selected></option><option>Yes, Love it!</option><option>Yes, they can be fun</option><option>Sometimes</option><option>No, I like to do my own thing </option> <option>I hate it</option> 
                     </select>
                 </label>
                 <br />
                 <label>Do you believe others have good intentions?
-                    <select class="form-control" id="a17" name="a17" onChange={this.handleChange} value={this.state.a17}>
+                    <select class="form-control" id="pa17" name="pa17" onChange={this.handleChange} value={this.state.pa17}>
                         <option selected></option><option>Yes!</option><option>Most people</option><option>Not sure</option><option>No, a few do</option> <option>No, I don't trust anyone</option> 
                     </select>
                 </label>
