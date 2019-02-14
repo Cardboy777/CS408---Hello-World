65506af5739e4ad373a2013b88577a8e0abca8ba
lkristie <lauragkristie96@gmail.com>
2019-02-14 17:12:46 -0500

    Changed Coding Questionnaire Variable Names

--- client/src/CodingQnn.js
+++ client/src/CodingQnn.js
@@ -9,16 +9,16 @@ class CodingQuestionnaire extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            a1:"",
-            a2:"",
-            a3:"",
-            a4:"",
-            a5:"",
-            a6:"",
-            a7:"",
-            a8:"",
-            a9:"",
-            a10:"",
+            ca1:"",
+            ca2:"",
+            ca3:"",
+            ca4:"",
+            ca5:"",
+            ca6:"",
+            ca7:"",
+            ca8:"",
+            ca9:"",
+            ca10:"",
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -33,28 +33,28 @@ class CodingQuestionnaire extends Component {
             return;
         }
         const questionnaireRef=db.collection("usersPQ").add({
-            answer1: this.state.a1,
-            answer2: this.state.a2,
-            answer3: this.state.a3,
-            answer4: this.state.a4,
-            answer5: this.state.a5,
-            answer6: this.state.a6,
-            answer7: this.state.a7,
-            answer8: this.state.a8,
-            answer9: this.state.a9,
-            answer10: this.state.a10
+            canswer1: this.state.ca1,
+            canswer2: this.state.ca2,
+            canswer3: this.state.ca3,
+            canswer4: this.state.ca4,
+            canswer5: this.state.ca5,
+            canswer6: this.state.ca6,
+            canswer7: this.state.ca7,
+            canswer8: this.state.ca8,
+            canswer9: this.state.ca9,
+            canswer10: this.state.ca10
         });
         this.setState({
-            a1:"",
-            a2:"",
-            a3:"",
-            a4:"",
-            a5:"",
-            a6:"",
-            a7:"",
-            a8:"",
-            a9:"",
-            a10:""
+            ca1:"",
+            ca2:"",
+            ca3:"",
+            ca4:"",
+            ca5:"",
+            ca6:"",
+            ca7:"",
+            ca8:"",
+            ca9:"",
+            ca10:""
         });
     }
 
@@ -94,16 +94,16 @@ class CodingQuestionnaire extends Component {
         });
     }
     handleValidation(){
-        if(this.state.a1===""){ window.alert("Invalid Question Input"); return false;}
-        else if(this.state.a2===""){ window.alert("Invalid Question Input"); return false;}
-        else if(this.state.a3===""){ window.alert("Invalid Question Input"); return false;}
-        else if(this.state.a4===""){ window.alert("Invalid Question Input"); return false;}
-        else if(this.state.a5===""){ window.alert("Invalid Question Input"); return false;}
-        else if(this.state.a6===""){ window.alert("Invalid Question Input"); return false;}
-        else if(this.state.a7===""){ window.alert("Invalid Question Input"); return false;}
-        else if(this.state.a8===""){ window.alert("Invalid Question Input"); return false;}
-        else if(this.state.a9===""){ window.alert("Invalid Question Input"); return false;}
-        else if(this.state.a10===""){ window.alert("Invalid Question Input"); return false;}
+        if(this.state.ca1===""){ window.alert("Invalid Question Input"); return false;}
+        else if(this.state.ca2===""){ window.alert("Invalid Question Input"); return false;}
+        else if(this.state.ca3===""){ window.alert("Invalid Question Input"); return false;}
+        else if(this.state.ca4===""){ window.alert("Invalid Question Input"); return false;}
+        else if(this.state.ca5===""){ window.alert("Invalid Question Input"); return false;}
+        else if(this.state.ca6===""){ window.alert("Invalid Question Input"); return false;}
+        else if(this.state.ca7===""){ window.alert("Invalid Question Input"); return false;}
+        else if(this.state.ca8===""){ window.alert("Invalid Question Input"); return false;}
+        else if(this.state.ca9===""){ window.alert("Invalid Question Input"); return false;}
+        else if(this.state.ca10===""){ window.alert("Invalid Question Input"); return false;}
         else{ return true; }
         
     }
@@ -147,14 +147,14 @@ class CodingQuestionnaire extends Component {
                 </div>
                 <div class="form-row">
                 <label>What motivates you to code?
-                    <select class="form-control" id="a1" name="a1" onChange={this.handleChange} value={this.state.a1}>
+                    <select class="form-control" id="ca1" name="ca1" onChange={this.handleChange} value={this.state.ca1}>
                         <option selected></option><option>Inner Peace</option> <option>Success</option> <option>Fun</option>
                         <option>Power</option> <option>The Money</option>
                     </select>
                 </label>
                 <br />
                 <label>What is your favorite coding langugage
-                    <select class="form-control" id="a2" name="a2" onChange={this.handleChange} value={this.state.a2}>
+                    <select class="form-control" id="ca2" name="ca2" onChange={this.handleChange} value={this.state.ca2}>
                         <option selected></option><option>C</option> <option>C++</option> <option>Python</option>
                         <option>Java</option> <option>Matlab</option><option>HTML</option> <option>R</option> <option>Visual Basic</option><option>Fortran</option>
                         <option>Javascript</option> <option>C#</option><option>Ruby</option>
@@ -162,39 +162,39 @@ class CodingQuestionnaire extends Component {
                 </label>
                 <br />
                 <label> What is your favorite search algorithm?
-                    <select class="form-control" id="a3" name="a3" onChange={this.handleChange} value={this.state.a3}>
+                    <select class="form-control" id="ca3" name="ca3" onChange={this.handleChange} value={this.state.ca3}>
                         <option selected></option><option>Sequential </option><option>Interval</option> <option>Binary</option>
                         <option>Linear</option> <option> Exponential</option> <option>Recursive</option>
                     </select>
                 </label>
                 <br />
                 <label>Do you enjoy working in groups?
-                    <select class="form-control" id="a4"name="a4" onChange={this.handleChange} value={this.state.a4}>
+                    <select class="form-control" id="ca4"name="ca4" onChange={this.handleChange} value={this.state.ca4}>
                         <option selected></option><option>Yes</option><option>No</option> 
                     </select>
                 </label>
                 <br />
                 <label>IOS: Swift or Android Development?
-                    <select class="form-control" id="a5" name="a5" onChange={this.handleChange} value={this.state.a5}>
+                    <select class="form-control" id="ca5" name="ca5" onChange={this.handleChange} value={this.state.ca5}>
                         <option selected></option><option>Swift</option><option>Android</option> 
                     </select>            
                 </label>
                 <br />
                 <label>What is your favorite IDE?
-                    <select class="form-control" id="a6" name="a6" onChange={this.handleChange} value={this.state.a6}>
+                    <select class="form-control" id="ca6" name="ca6" onChange={this.handleChange} value={this.state.ca6}>
                         <option selected></option><option>Vim</option><option>Emacs</option><option>IntelliJ</option> 
                         <option>Eclipse</option> <option>Visual Studio Code</option> <option>CodeBlocks</option> <option>Other</option>
                     </select>
                 </label>
                 <br />
                 <label> How do you feel about web development?
-                    <select class="form-control" id="a7" name="a7" onChange={this.handleChange} value={this.state.a7}>
+                    <select class="form-control" id="ca7" name="ca7" onChange={this.handleChange} value={this.state.ca7}>
                         <option selected></option><option>Yes, Love it!</option> <option>Not really</option> <option>I hate it</option> 
                     </select>
                 </label>
                 <br />
                 <label> When do you like to code the most?
-                    <select class="form-control" id="a8" name="a8" onChange={this.handleChange} value={this.state.a8}>
+                    <select class="form-control" id="ca8" name="ca8" onChange={this.handleChange} value={this.state.ca8}>
                         <option selected></option>
                         <option>Early Morning</option><option>Late Night</option><option>Between Tasks</option>
                         <option>Whenever Inspiration hits</option> <option>When I have planed a specific time to work on it</option>
@@ -203,14 +203,14 @@ class CodingQuestionnaire extends Component {
                 </label>
                 <br />
                 <label>Do you enjoy participating in Hackathons?
-                    <select class="form-control" id="a9" name="a9" onChange={this.handleChange} value={this.state.a9}>
+                    <select class="form-control" id="ca9" name="ca9" onChange={this.handleChange} value={this.state.ca9}>
                         <option selected></option><option>Yes, always</option><option>Most of the time</option><option> Sometimes</option> 
                         <option>Not so much, but I try too</option> <option>No</option>
                     </select>
                 </label>
                 <br />
                 <label>Favorite Data Structure?
-                    <select class="form-control" id="a10" name="a10" onChange={this.handleChange} value={this.state.a10}>
+                    <select class="form-control" id="ca10" name="ca10" onChange={this.handleChange} value={this.state.ca10}>
                         <option selected></option><option>Arrays</option><option>Linked Lists</option> <option>Trees</option>
                         <option>Stack</option> <option>Queue</option><option>Hashing</option> <option>Heap</option> <option>Graph</option> <option>C++ Vector</option>
                     </select>
