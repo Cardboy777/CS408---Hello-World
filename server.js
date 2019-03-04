35022b62ca624fea8ca6e9b94858e685098090bb
Reid Kippenbrock <mcreido@gmail.com>
2019-03-03 19:21:29 -0500

    Matching Page (Pretty Much) Complete

--- server.js
+++ server.js
@@ -460,7 +460,6 @@ admin.initializeApp({
 router.post("/getMorePotentialMatches", (req, res) => {
   result = findMatches(req.body.username);
   result.then(function(ret){
-    console.log(ret);
     res.json(ret);
   })
 });
@@ -468,7 +467,6 @@ router.post("/getMorePotentialMatches", (req, res) => {
 router.post("/getMatches", (req, res) => {
   result = getMatches(req.body.username);
   result.then(function(ret){
-    console.log(ret);
     res.json(ret);
   })
 });
@@ -478,7 +476,7 @@ router.post("/likeUser", (req, res) => {
   console.log(req.body.userName + " Liked " + req.body.likedUserName);
   result = likeUser(req.body.userName, req.body.likedUserName);
   result.then(function(ret){
-    res.json("Success");
+    res.json(ret);
   })
 });
 
@@ -486,7 +484,7 @@ router.post("/dislikeUser", (req, res) => {
   console.log(req.body.userName + " Disiked " + req.body.dislikedUserName);
   result = dislikeUser(req.body.userName, req.body.dislikedUserName);
   result.then(function(ret){
-    res.json("Success");
+    res.json(ret);
   })
 });
 
@@ -494,7 +492,7 @@ router.post("/unlikeUser", (req, res) => {
   console.log(req.body.userName + " Unliked " + req.body.unlikedUserName);
   result = removeMatch(req.body.userName, req.body.dislikedUserName);
   result.then(function(ret){
-    res.json("Success");
+    res.json(ret);
   })
 });
 
