1cd9e9d7345efb3114355a07d131bd383d64e34b
Shivan Desai <desai58@purdue.edu>
2019-03-10 17:49:26 -0400

    Added messages

--- client/src/Messages.js
+++ client/src/Messages.js
@@ -1,62 +1,229 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import './css/Messages.css'
+import ChatBoard from './chatBoard'
+import { myFirebase, myFirestore } from './firebase'
 
-import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:8080');
+// import openSocket from 'socket.io-client';
+// const socket = openSocket('http://localhost:8080');
 
 class Messages extends Component {
-	constructor(){
-        super();
-		this.sendMessage = this.sendMessage.bind(this);
-		this.checkForMessages();
-	}
-	
-	checkForMessages()
-	{
-		window.localStorage.setItem("unreadMessageCount", "0");
-		socket.on('sendMessageToUserResponse', function(resp)
-		{
-			window.alert(resp);
-		});
-		
-		socket.on('receiveMessage', function(msg)
-		{
-			window.alert("Receiving: " + JSON.stringify(msg));
-		});
-		
-		socket.emit('testMessageClientToServer', "FWFGWFWE");
-	}
-	
-	sendMessage(e)
-	{
-		e.preventDefault();
-		var currentUser = JSON.parse(window.localStorage.getItem("user"));
-		var userEmail = currentUser.email;
-		var userKey = currentUser.uid;
-		
-		//var messageText = document.getElementById("messageTextBox").value;
-		var messageObject = {};
-		messageObject.sender = {"email":userEmail, "uid":userKey};
-		messageObject.receiver = "paultest@test.com";
-		messageObject.id = Math.random();
-		messageObject.message = "TEST";
-		
-		socket.emit("sendMessageToUser", "FAF"); //messageObject);
-		
-	}
-	
+	constructor(props) {
+    super(props)
+    this.state = {
+      isLoading: true,
+      isOpenDialogConfirmLogout: false,
+      currentPeerUser: null
+    }
+    this.currentUserId = JSON.parse(window.localStorage.getItem("user"));
+    // this.currentUserAvatar = localStorage.getItem(AppString.PHOTO_URL)
+    this.currentUserNickname = JSON.parse(window.localStorage.getItem("user"));
+    this.listUser = []
+	this.viewListUser1 = []
+  }
+
+  componentDidMount() {
+    // this.checkLogin()
+	 // this.getListUser()
+	 this.renderListUser();
+  }
+
+
+  // checkLogin = () => {
+  //   if (!localStorage.getItem(AppString.ID)) {
+  //     this.setState({ isLoading: false }, () => {
+  //       this.props.history.push('/')
+  //     })
+  //   } else {
+  //     this.getListUser()
+  //   }
+  // }
+
+  // getListUser = sync () => {
+    // myFirestore.collection("usersPQ").doc("LZ4cQ4PkDgbVLexbavYXmmpzofk1").onSnapshot(function(doc) {
+    //     console.log("Current data: ", doc.data());
+	// 	this.listUser = doc.data().prevMatchedUsers;
+	// 	console.log(this.listUser);
+    // });
+
+
+    // if (result.docs.length > 0) {
+    //   this.listUser = [...result.docs]
+    //   this.setState({ isLoading: false })
+    // }
+  // }
+
+  // onLogoutClick = () => {
+  //   this.setState({
+  //     isOpenDialogConfirmLogout: true
+  //   })
+  // }
+
+  // doLogout = () => {
+  //   this.setState({ isLoading: true })
+  //   myFirebase
+  //     .auth()
+  //     .signOut()
+  //     .then(() => {
+  //       this.setState({ isLoading: false }, () => {
+  //         localStorage.clear()
+  //         this.props.showToast(1, 'Logout success')
+  //         this.props.history.push('/')
+  //       })
+  //     })
+  //     .catch(function(err) {
+  //       this.setState({ isLoading: false })
+  //       this.props.showToast(0, err.message)
+  //     })
+  // }
+
+  // hideDialogConfirmLogout = () => {
+  //   this.setState({
+  //     isOpenDialogConfirmLogout: false
+  //   })
+  // }
+
+  // onProfileClick = () => {
+  //   this.props.history.push('/profile')
+  // }
+
+  renderListUser = async () => {
+	// this.getListUser();
+	var that = this;
+	console.log("in rlu");
+	console.log(this.currentUserId);
+	myFirestore.collection("usersPQ").doc("mLUGrX6hNuUTII2DlcBywewHVd63").onSnapshot(function(doc) {
+	  	console.log("Current data: ", doc.data());
+	  	this.listUser = doc.data().matchedUsers;
+	  	console.log(this.listUser);
+	  	console.log("Inside renderlistuser1-----" + this.listUser);
+    	if (this.listUser.length > 0) {
+			console.log("Inside renderlistuser");
+      	 	let viewListUser = []
+      		this.listUser.forEach((item, index) => {
+				console.log("inside for each!!!" + item);
+        		if (item.uid !== this.currentUserId) {
+					console.log("inside if");
+          			viewListUser.push(
+	            		<button
+							className = 'viewWrapItemFocused'
+	              			// className={
+	              			//   this.state.currentPeerUser &&
+	              			//   this.state.currentPeerUser === item
+	              			//     ? 'viewWrapItemFocused'
+	              			//     : 'viewWrapItem'
+	              			// }
+	              			key={item.uid}
+	              			onClick={() => {
+	                			that.setState({ currentPeerUser: item })
+	              			}}
+	            		>
+			              {/* <img
+			                className="viewAvatarItem"
+			                src={item.data().photoUrl}
+			                alt="icon avatar"
+			              /> */}
+				              <div className="viewWrapContentItem">
+				                <span className="textItem">{
+				                  item.user
+				                }</span>
+				              </div>
+			            </button>
+          			)
+		  			console.log(viewListUser);
+        		}
+      		});
+			console.log("outside for each!");
+			console.log(viewListUser[0]);
+			that.setState({
+				viewListUser1: viewListUser
+			});
+      		return viewListUser;
+    	} else {
+      		return null
+    	}
+	});
+  }
+
   render() {
     return (
-      <div>
-        <Header {...this.props}/>
-        <h1>Messages Page</h1>
-		<div id="messageBoxDiv">
-			<input id="messageTextBox" placeholder="Type your message here."></input>
-			<button id="sendMessageButton" onClick={this.sendMessage}>Send</button>
-		</div>
+      <div className="root1">
+		  <Header/>
+		  {console.log("below header!")}
+		  {console.log(this.state.currentPeerUser)}
+        {/* Header */}
+        {/* <div className="header">
+          <span>MAIN</span>
+          <img
+            className="icProfile"
+            alt="An icon default avatar"
+            src={images.ic_default_avatar}
+            onClick={this.onProfileClick}
+          />
+          <img
+            className="icLogout"
+            alt="An icon logout"
+            src={images.ic_logout}
+            onClick={this.onLogoutClick}
+          />
+        </div> */}
+
+        {/* Body */}
+        <div className="body">
+          <div className="viewListUser"> {this.state.viewListUser1}</div>
+
+          <div className="viewBoard">
+             {this.state.currentPeerUser ? (
+              <ChatBoard
+                currentPeerUser={this.state.currentPeerUser}
+              />
+            ) : (
+				<h1>hi</h1>
+              // <WelcomeBoard
+              //   currentUserNickname={this.currentUserNickname}
+              //   currentUserAvatar={this.currentUserAvatar}
+              // />
+            )}
+           </div>
+        </div>
+
+        {/* Dialog confirm */}
+        {/* {this.state.isOpenDialogConfirmLogout ? (
+          <div className="viewCoverScreen">
+            {this.renderDialogConfirmLogout()}
+          </div>
+        ) : null} */}
+
+        {/* Loading */}
+        {/* {this.state.isLoading ? (
+          <div className="viewLoading">
+            <ReactLoading
+              type={'spin'}
+              color={'#203152'}
+              height={'3%'}
+              width={'3%'}
+            />
+          </div>
+        ) : null} */}
       </div>
-    );
+    )
   }
+
+  // renderDialogConfirmLogout = () => {
+  //   return (
+  //     <div>
+  //       <div className="titleDialogConfirmLogout">Are you sure to logout?</div>
+  //       <div className="viewWrapButtonDialogConfirmLogout">
+  //         <button className="btnYes" onClick={this.doLogout}>
+  //           YES
+  //         </button>
+  //         <button className="btnNo" onClick={this.hideDialogConfirmLogout}>
+  //           CANCEL
+  //         </button>
+  //       </div>
+  //     </div>
+  //   )
+  // }
 }
 
 export default Messages;
